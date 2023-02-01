9bc5a67b4cf8c9157ae9021b50997ee7b44ae215
mdowns <michaeljdowns99@gmail.com>
2023-02-01 15:41:36 -0700

    Made Movie js file

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 
 
 const controller = require('../controller/index');
-router.get('/movie', controller.getAllMovies);
-router.get('/movie/:id', controller.getMovieByID);
-router.post('/movie', controller.postNewMovie);
+// router.get('/movie', controller.getAllMovies);
+// router.get('/movie/:id', controller.getMovieByID);
+// router.post('/movie', controller.postNewMovie);
+router.use('/movie', require('./movie'));
 
 router.use('/', require('./swagger'));
-//router.get('/movie', require('./movie'));
 
 module.exports = router;
--- routes/movie.js
+++ routes/movie.js
@@ -1,10 +1,10 @@
-// const routes = require('express').Router();
-// const controller = require('../controller/index');
+const routes = require('express').Router();
+const controller = require('../controller/index');
 
 
-// routes.get('/', controller.getAllMovies);
-// routes.get('/:id', controller.getMovieByID);
-// routes.post('/', controller.postNewMovie);
+routes.get('/', controller.getAllMovies);
+routes.get('/:id', controller.getMovieByID);
+routes.post('/', controller.postNewMovie);
 
 
-// module.exports = routes;
+module.exports = routes;
