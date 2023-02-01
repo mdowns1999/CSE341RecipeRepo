fb61110c9ab87ba1c9b17afc38667479ee2be7e0
mdowns <michaeljdowns99@gmail.com>
2023-02-01 12:49:17 -0700

    Add Swagger for Render

--- swagger.js
+++ swagger.js
@@ -5,15 +5,12 @@ const doc = {
     title: 'My API',
     description: 'Contacts'
   },
-  host: 'localHost:8080',
+  host: 'downsrecipes.onrender.com',
   schemes: ['http']
 };
 
 const outputFile = './swagger.json';
 const endpointsFiles = ['./routes/index.js'];
 
-/* NOTE: if you use the express Router, you must pass in the 
-   'endpointsFiles' only the root file where the route starts,
-   such as index.js, app.js, routes.js, ... */
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
